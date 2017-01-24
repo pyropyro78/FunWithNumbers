13c4362330f8926c4c7f88c84782b0fa8007620e
Bradley Van Dyne <pyropyro78@gmail.com>
2017-01-23 17:00:28 -0800

    First stable version

--- src/com/pyromanticgaming/funwithnumbers/FunWithNumbers.java
+++ src/com/pyromanticgaming/funwithnumbers/FunWithNumbers.java
@@ -1,11 +1,11 @@
 package com.pyromanticgaming.funwithnumbers;
 
 /*
-*Copyright (c) <2017>, <pyropyro78>, <pyropyro78@gmail.com>
-*All rights reserved.
-*
-*THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ *Copyright (c) <2017>, <pyropyro78>, <pyropyro78@gmail.com>
+ *All rights reserved.
+ *
+ *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
@@ -43,9 +43,9 @@ public class FunWithNumbers extends JavaPlugin implements Listener {
 			@Override
 			public void run() {
 				for (Player player1 : Bukkit.getServer().getOnlinePlayers()) {
-					
+
 					NumberCruncher.addTime(player1.getName());
-					
+
 				}
 			}
 		}, 0L, 20L);
@@ -59,32 +59,34 @@ public class FunWithNumbers extends JavaPlugin implements Listener {
 
 			}
 		}, MainConfig.SaveTimer, MainConfig.SaveTimer);
-		
+
 	}
 
 	@Override
 	public void onDisable() {
 		NumberCruncherConfig.savenumbers();
 		getLogger().info("Fun With Numbers has been disabled.");
-		
+
 	}
 
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event) {
 		String p = event.getPlayer().getName();
-		
+
 		if (NumberCruncher.hasJoined(p)) {
-			
+
 			NumberCruncher.addJoins(p);
+		} else {
+
+			NumberCruncher.joins.put(p, 1);
+			NumberCruncher.blocksb.put(p, 0);
+			NumberCruncher.blocksp.put(p, 0);
+			NumberCruncher.deaths.put(p, 0);
+			NumberCruncher.kills.put(p, 0);
+			NumberCruncher.playtime.put(p, 0);
+
 		}
-		
-		NumberCruncher.joins.put(p, 1);
-		NumberCruncher.blocksb.put(p, 0);
-		NumberCruncher.blocksp.put(p, 0);
-		NumberCruncher.deaths.put(p, 0);
-		NumberCruncher.kills.put(p, 0);
-		NumberCruncher.playtime.put(p, 0);
-		
+
 	}
 
 	@EventHandler
@@ -92,7 +94,7 @@ public class FunWithNumbers extends JavaPlugin implements Listener {
 		if (e.getEntity().getKiller() instanceof Player) {
 			Player victim = e.getEntity();
 			Player killer = e.getEntity().getKiller();
-			
+
 			NumberCruncher.addDeath(victim.getName());
 			NumberCruncher.addKill(killer.getName());
 
@@ -105,17 +107,17 @@ public class FunWithNumbers extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event) {
 		Player p = (Player) event.getPlayer();
-		
+
 		NumberCruncher.addBlocksBroken(p.getName());
-		
+
 	}
 
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent event) {
 		Player p = (Player) event.getPlayer();
-		
+
 		NumberCruncher.addBlocksPlaced(p.getName());
-		
+
 	}
 
 }
